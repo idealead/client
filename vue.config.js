a940f28521eeefce7b5c46d0075e790b8d065e93
lucaslow <974860016@qq.com>
2020-01-14 16:24:47 +0800

    修改port

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	devServer: {
 	    open: true, //是否自动弹出浏览器页面
 	    host: "localhost",
-	    port: '8080',
+	    port: '80',
 	    https: false,
 	    hotOnly: false,
 	    proxy: {
