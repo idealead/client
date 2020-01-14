5cfd466a358ba338f1634e3a97dc6fca71262352
lucaslow <974860016@qq.com>
2020-01-14 16:11:54 +0800

    修改config 1

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	    hotOnly: false,
 	    proxy: {
 		  '/api': {
-	        target: 'http://dev.cyrd.gdinsight.com', //API测试服务器的地址 123
+	        target: 'http://dev.cyrd.gdinsight.com', //API测试服务器的地址
 	        changeOrigin: true,
 	        pathRewrite: {'^/api': ''}
         }	
